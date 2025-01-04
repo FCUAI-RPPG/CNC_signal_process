7c8225a5613582d01b3a031cfd75fd10a3556f3d
FCUAI-RPPG <123925957+FCUAI-RPPG@users.noreply.github.com>
2025-01-04 15:32:27 +0800

    Update __init__.py

--- config/__init__.py
+++ config/__init__.py
@@ -6,7 +6,11 @@ def get_cfg_defaults():
   # This is for the "local variable" use pattern
   return cfg.clone()
 
+import os
 from config import get_cfg_defaults
+
+base_dir = os.path.dirname(os.path.abspath(__file__))  # 獲取當前腳本所在目錄
+config_path = os.path.join(base_dir, "config.yml")  # 拼接路徑
 cfg = get_cfg_defaults()
-cfg.merge_from_file("CNC_signal_process/config/config.yml")
+cfg.merge_from_file(config_path)
 cfg.freeze()
